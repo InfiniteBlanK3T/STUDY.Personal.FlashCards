1f5b7bc7bc36bedda81985cd4733939992fa324e
Thomas <94949422+InfiniteBlanK3T@users.noreply.github.com>
2023-02-13 20:18:46 +1100

    Connection to DB established

--- FlashCards/Program.cs
+++ FlashCards/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Flashcards;
+
+class Program
+{
+    static void Main()
+    {
+        //string connectionString = ConfigurationManager.ConnectionStrings["connectionstring"].ConnectionString;
+        string connectionString = "Data Source=(LocalDb)\\PersonalProjectDB;Initial Catalog=FlashcardsDB";
+        using var connection = new SqlConnection(connectionString);
+        connection.Open();
+        var tableCmd = connection.CreateCommand();
+        tableCmd.CommandText =
+            $@"IF NOT EXISTS (SELECT * FROM sys.tables t
+            JOIN sys.schemas s ON (t.schema_id = s.schema_id) 
+            WHERE s.name = 'dbo' AND t.name = 't1')
+            CREATE TABLE TestTABLE (
+            Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+            name VARCHAR(55)
+            );";
+        try
+        {
+            tableCmd.ExecuteNonQuery();
+            Console.WriteLine("Succeed!");
+            Console.ReadLine();
+        }
+        catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+        Console.ReadLine();
+    }
+}
